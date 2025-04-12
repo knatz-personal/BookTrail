db97e25ed35d86ec807319a2a2f5553d91941043
Nathan Z Khupe <nathan.khupe@agilis.com.mt>
2025-04-12 22:48:43 +0200

    Versioning

---
+++ src/BookTrail.API/Features/TodoEndpointDefinitions.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Versioning;
+
+namespace BookTrail.API.Features
+{
+    public static class TodoEndpointDefinitions
+    {
+        public static void MapV1TodosEndpoints(this RouteGroupBuilder @this)
+        {
+            ArgumentNullException.ThrowIfNull(@this, nameof(@this));
+            var group = @this.MapGroup("Todos")
+                .WithTags("Todos")
+                .WithDisplayName("Todos")
+                .MapToApiVersion(new ApiVersion(1, 0));
+
+
+            var sampleTodos = new Todo[]
+            {
+                        new(1, "Walk the dog"),
+                        new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+                        new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+                        new(4, "Clean the bathroom"),
+                        new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+            };
+
+            group.MapGet("", () => sampleTodos);
+            group.MapGet("{id}", (int id) =>
+                sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+                    ? Results.Ok(todo)
+                    : Results.NotFound());
+        }
+    }
+}
--- src/BookTrail.API/Program.cs
+++ src/BookTrail.API/Program.cs
@@ -1,5 +1,8 @@
 using Asp.Versioning;
+using Asp.Versioning.Builder;
 using BookTrail.API;
+using BookTrail.API.Features;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
 
@@ -22,45 +25,44 @@ builder.Services.AddOpenApi(options =>
     });
 });
 
+var defaultVersion = new ApiVersion(1, 0);
+
 // Add API versioning
 builder.Services.AddApiVersioning(options =>
 {
     options.AssumeDefaultVersionWhenUnspecified = true;
-    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.DefaultApiVersion = defaultVersion;
     options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+     new UrlSegmentApiVersionReader(),
+     new HeaderApiVersionReader("X-Api-Version"));
+
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
 });
 
+
 var app = builder.Build();
+
+// Define a set of supported API versions
+var apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(defaultVersion)
+    .HasApiVersion(new ApiVersion(1, 1))
+    .ReportApiVersions()
+    .Build();
+
 
 app.MapOpenApi();
 app.MapScalarApiReference();
 
-var apiRoot = app.MapGroup("/api");
+// Define the API root with a version placeholder
+var apiRoot = app.MapGroup("/api/v{version:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);
 apiRoot.MapOpenApi();
 
-// Version 1.0 routes
-var v1 = apiRoot.MapGroup("/v1");
-
-MapTodosEndpoints(v1);
+apiRoot.MapV1TodosEndpoints();
 
 await app.RunAsync();
 
-
-// Helper method to map todos endpoints
-void MapTodosEndpoints(RouteGroupBuilder group)
-{
-    var sampleTodos = new Todo[]
-    {
-        new(1, "Walk the dog"),
-        new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-        new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-        new(4, "Clean the bathroom"),
-        new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-    };
-
-    group.MapGet("/todos", () => sampleTodos);
-    group.MapGet("/todos/{id}", (int id) =>
-        sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
-}
